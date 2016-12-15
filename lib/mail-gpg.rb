ba504a3aeaa3afae49a611f845e70206e519a7b9
Tobias <tov@nrw.net>
2016-12-15 16:02:12 +0100

    Object.blank

--- lib/mail-gpg.rb
+++ lib/mail-gpg.rb
@@ -1,34 +1,46 @@
 require 'mail/gpg'
 
 module Extensions
-	#just copied from Rails
-	module String
-		BLANK_RE = /\A[[:space:]]*\z/
+  #just copied from Rails
+  module String
+    BLANK_RE = /\A[[:space:]]*\z/
 
-		# A string is blank if it's empty or contains whitespaces only:
-		#
-		#   ''.blank?       # => true
-		#   '   '.blank?    # => true
-		#   "\t\n\r".blank? # => true
-		#   ' blah '.blank? # => false
-		#
-		# Unicode whitespace is supported:
-		#
-		#   "\u00a0".blank? # => true
-		#
-		# @return [true, false]
-		def blank?
-			# The regexp that matches blank strings is expensive. For the case of empty
-			# strings we can speed up this method (~3.5x) with an empty? call. The
-			# penalty for the rest of strings is marginal.
-			empty? || BLANK_RE === self
-		end 
-	end
+    # A string is blank if it's empty or contains whitespaces only:
+    #
+    #   ''.blank?       # => true
+    #   '   '.blank?    # => true
+    #   "\t\n\r".blank? # => true
+    #   ' blah '.blank? # => false
+    #
+    # Unicode whitespace is supported:
+    #
+    #   "\u00a0".blank? # => true
+    #
+    # @return [true, false]
+    def blank?
+      # The regexp that matches blank strings is expensive. For the case of empty
+      # strings we can speed up this method (~3.5x) with an empty? call. The
+      # penalty for the rest of strings is marginal.
+      empty? || BLANK_RE === self
+    end 
+  end
+
+  module Object
+    def blank?
+      respond_to?(:empty?) ? !!empty? : !self
+    end
+  end
 end
 
 if String.new.respond_to? :'blank?'
-	#nop
+  #nop
 else
    String.include Extensions::String
 end
 
+
+if Object.new.respond_to? :'blank?'
+  #nop
+else
+   Object.include Extensions::Object
+end
